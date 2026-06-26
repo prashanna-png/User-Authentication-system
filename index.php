49e6ac5cb263b4c7994afb9a21b99056a1959ee2
prashanna <prashanna9742@gmail.com>
2026-06-26 20:15:42 +0545

    add index file

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>User Authentication System</title>
+  <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+  <div class="container">
+    <h1>User Authentication System</h1>
+    <form action="register.php">
+      <button type="submit" class="create">Create Account</button>
+    </form>
+    <p>or</p>
+    <form action="login.php">
+      <button type="submit" class="login">Login</button>
+    </form>
+  </div>
+</body>
+</html>
